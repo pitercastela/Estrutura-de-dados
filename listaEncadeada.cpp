4d1f9e6df2d681c063f8fb1da79b8e7b4f83478b
Lucas Alves <lucasacastela@gmail.com>
2025-10-25 22:22:35 -0300

    lista de exercicios parte 1

--- listaEncadeada.cpp
+++ listaEncadeada.cpp
@@ -1,4 +1,3 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
@@ -44,32 +43,135 @@ class ListaEncadiada{public:
         }
         if (p->proximo != nullptr) {
             No *remover = p->proximo;
-            p->proximo = remover->proximo; 
+            p->proximo = remover->proximo;
             delete remover;
-            delete p;
         }
     };
-        void Busca(int info){
-            No *p = head;
-            while(p != nullptr){
-                if(p->dado == info){
-                    cout << "Elemento " << info << " encontrado na lista." << endl;
-                    return;
-                }
+        bool Busca(int info){
+            No *p = head->proximo;
+            while(p->proximo != nullptr && p->dado != info){
                 p = p->proximo;
             }
-            cout << "Elemento " << info << " nao encontrado na lista." << endl;
+            if (p->dado == info)
+            {
+                return true;
+            } return false;
+
         }
-    };
+        int Conta(){
+            int num = 0;
+            No *p = head->proximo;
+            while (p != nullptr)
+            {
+                num += 1;
+                p = p->proximo;
+            }
+            return num;
+        }
+        void inverter() {
+        No *anterior = nullptr;
+        No *atual = head->proximo;
+        No *proximo = nullptr;
+
+        while (atual != nullptr) {
+            proximo = atual->proximo;
+
+            atual->proximo = anterior;
+
+            anterior = atual;
+            atual = proximo;
+        }
+
+        head->proximo = anterior;
+    }
+
+    void imprimir() {
+        No *p = head->proximo;
+        cout << "Lista: [ ";
+        while (p != nullptr) {
+            cout << p->dado << " ";
+            p = p->proximo;
+        }
+        cout << "]" << endl;
+    }
+    void removerDuplicatas() {
+        No *p = head->proximo;
+
+        while (p != nullptr) {
+            No *aux = p;
+
+            while (aux->proximo != nullptr) {
+
+                if (aux->proximo->dado == p->dado) {
+                    No *noRemover = aux->proximo;
+
+                    aux->proximo = noRemover->proximo;
+
+                    delete noRemover;
+                } else {
+                    aux = aux->proximo;
+                }
+            }
+
+            p = p->proximo;
+        }
+    }
+    ListaEncadiada mesclar(ListaEncadiada& outraLista) {
+        ListaEncadiada listaMesclada;
+        No* tail = listaMesclada.head; // Ponteiro para o fim da nova lista
+
+        // 1. Copia todos os elementos da lista ATUAL (this)
+        No* p = this->head->proximo;
+        while (p != nullptr) {
+            tail->proximo = new No(p->dado); // Cria novo nó e anexa
+            tail = tail->proximo;            // Avança o ponteiro tail
+            p = p->proximo;                   // Avança o ponteiro da lista atual
+        }
+
+        // 2. Copia todos os elementos da OUTRA lista (outraLista)
+        p = outraLista.head->proximo;
+        while (p != nullptr) {
+            tail->proximo = new No(p->dado); // Cria novo nó e anexa
+            tail = tail->proximo;            // Avança o ponteiro tail
+            p = p->proximo;                   // Avança o ponteiro da outra lista
+        }
+
+        return listaMesclada; // Retorna a nova lista completa
+    }
+    void selectionSort() {
+        No* atual = head->proximo;
+        while (atual != nullptr) {
+            No* minNo = atual;
+            No* busca = atual->proximo;
+
+            while (busca != nullptr) {
+                if (busca->dado < minNo->dado) {
+                    minNo = busca;
+                }
+                busca = busca->proximo;
+            }
+            if (minNo != atual) {
+                int temp = atual->dado;
+                atual->dado = minNo->dado;
+                minNo->dado = temp;
+            }
+            atual = atual->proximo;
+        }
+    }
+
+};
 
 
 int main() {
     ListaEncadiada lista;
-    lista.addInicio(10);
-    lista.addFim(20);
-    lista.Busca(1);
-
-
-    
+    lista.addInicio(10); // Lista: [ 10 ]
+    lista.addFim(20);    // Lista: [ 10 20 ]
+    lista.addFim(30);    // Lista: [ 10 20 30 ]
+    lista.addFim(40);    // Lista: [ 10 20 30 40 ]
+    lista.addFim(40);    // Lista: [ 10 20 30 40 ]
+    lista.addFim(40);    // Lista: [ 10 20 30 40 ]
+    lista.imprimir();
+    lista.removerDuplicatas();
+    lista.imprimir();
     return 0;
 }
